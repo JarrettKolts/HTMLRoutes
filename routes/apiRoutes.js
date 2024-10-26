df8cd626f022d6f9cda3201229c0e84f74a156a6
Jarrett Kolts <jarrettk51@gmail.com>
2024-10-26 17:27:33 -0500

    This seems to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,44 +1,40 @@
-const router = require('express').Router();
-const { readAndAppend, writeToFile, readFromFile } = require('../helpers/fsUtils');
-const { v4:uuidv4 } = require('uuid'); // For generating unique note IDs
-const path = require('path');
+const router = require("express").Router();
+const {
+  readFromFile, writeToFile, readAndAppend
+} = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid"); // For generating unique note IDs
+// const path = require("path");
 
 // TODO: Define the path to the database JSON file
-const filePath = path.join(__dirname, 'db/db.json');
+const filePath = "db/db.json";
 
-router.get('/notes', (req, res) => {
-    readFromFile(filePath)
+router.get("/notes", (req, res) => {
+  readFromFile(filePath)
     .then((data) => res.json(JSON.parse(data)))
     .catch((err) => {
       res.status(500).json(err);
     });
 });
 
+router.post("/notes", (req, res) => {
 
-router.post('/notes', (req, res) => {
-    // TODO: Destructure the title and text from the request body
-    const { title, text } = req.body;
-    // TODO: Return a 400 error if the title or text is missing
-if (!title || !text){
-  return res.status(400).json({error: 'Note title and text required!'})
-}
+  const { title, text } = req.body;
 
-const newNote = {
+  if (!title || !text) {
+    return res.status(400).json({ error: "Note title and text required!" });
+  }
+
+  const newNote = {
     title,
     text,
     id: uuidv4(),
-};
-    // TODO: Create a new note object with a unique id
+  };
 
-    // TODO: Use readAndAppend helper to add the new note to the JSON file
-
-    // TODO: Send the newly added note as a response
-    readAndAppend(newNote, filePath);
-    res.json(newNote);
+  readAndAppend(newNote, filePath);
+  res.json(newNote);
 });
 
-
-// BONUS 
+// BONUS
 // router.delete('/notes/:id', (req, res) => {
 //   // TODO: Get the ID of the note to delete from the request parameters
 
@@ -47,12 +43,12 @@ const newNote = {
 //   // TODO: Parse the data into an array of notes
 
 //   // TODO: Filter the notes array to remove the note with the matching ID
-     
+
 //   // TODO: Write the updated notes array back to the JSON file
-     
+
 //   // TODO: Send a response indicating the note was successfully deleted
 
 //   // TODO: Handle any errors that occur during the file operations
 // });
 
-module.exports = router; 
+module.exports = router;
